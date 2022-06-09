d84236c6bc517fa4d0fb8778ea9395217adebd82
Oreoluwa Ajao <39207128+oresam123@users.noreply.github.com>
2022-06-09 23:11:29 +0100

    beginning of api

---
+++ lib/utils/networking.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const String apiKey = '';
+const String url = '';
+
+class Networking {
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      return jsonDecode(data);
+    } else {
+      print(response.statusCode);
+    }
+  }
+}
