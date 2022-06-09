4de575f924b3f20dff2e3262c80ca8de581c1e77
Oreoluwa Ajao <39207128+oresam123@users.noreply.github.com>
2022-06-10 00:55:59 +0100

    new changes

--- lib/utils/networking.dart
+++ lib/utils/networking.dart
@@ -5,18 +5,18 @@ import 'package:geocoding/geocoding.dart';
 const String apiKey = '';
 late String destination;
 late String origin;
-const String url =
+final String url =
     'https://maps.googleapis.com/maps/api/distancematrix/json?destinations=$destination&origins=$origin&units=imperial&key=$apiKey';
 
 class Networking {
   Future getData() async {
     http.Response response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      String data = response.body;
-
+      var data = jsonDecode(response.body);
       return jsonDecode(data);
     } else {
       print(response.statusCode);
+      throw 'Problem with the get request';
     }
   }
 }
