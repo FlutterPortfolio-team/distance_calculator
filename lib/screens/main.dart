9272c2b8c1d1a2ea4565994b4e5532b820dedfed
victor-onoja <odohvictor47@gmail.com>
2022-06-10 15:46:54 +0100

    sign up and sign in ui

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,4 +1,5 @@
 import 'package:distance_calculator/screens/home_screen.dart';
+import 'package:distance_calculator/screens/sign_in.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,11 +9,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.dark(),
-      home: Scaffold(body: HomeScreen()),
+    return const MaterialApp(
+      title: 'Distance-Caclculator',
+      debugShowCheckedModeBanner: false,
+      home: SigninScreen(),
     );
   }
 }
